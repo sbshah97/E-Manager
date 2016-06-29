2c6b89d64f154ad89be4884772f3521f77e099e1
Salman Shah <sbs.191197@gmail.com>
2016-06-29 17:15:26 +0530

    COde changes made
    Added options for the code and created new functions for the same.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 #include<string>
 using namespace std;
 
+
+void newAccount();
+void editAccount(int, string);
+
 int main() {
 
     //Welcome Screen
@@ -25,6 +29,62 @@ int main() {
     cout << "1. Create new account" << endl;
     cout << "2. Edit existing account" << endl;
     cout << "3. Exit" << endl;
-    
+
+    //Choose options
+    int choice;
+    int accnum;
+    string pswd;
+    cin >> choice;
+
+    switch(choice) {
+
+        //Case to create new account
+        case 1:
+            newAccount();
+            break;
+
+
+        //Case to edit existing account
+        case 2:
+            cout << "Enter the account number: " << endl;
+            if(accnum == 123456) {
+                wrongpswd:
+                cout << "Enter your account's password" << endl;
+                cin >> pswd;
+                if (pswd == "nitk2015") {
+                    editAccount(accnum,pswd);
+                    }
+                else {
+                    cout << "Wrong Password entered" << endl;
+                    cout << "Try again" << endl;
+                    goto wrongpswd;
+                    }
+                }
+            else {
+                cout << "Account not found. Create a new account" << endl;
+                goto again;
+            }
+            break;
+
+        //Case to exit the program
+        case 3:
+            exit(0);
+
+        //In case none of the options are entered
+        default:
+            cout << "Enter an appropriate choice" <<
+            goto again;
+    }
+
     return 0;
+}
+
+//Function to add new account
+void newAccount() {
+    
+}
+
+//Function to edit existing account
+void existingAccount(int accnum, string pswd) {
+
 }
