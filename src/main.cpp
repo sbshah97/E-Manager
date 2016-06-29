9409959d3c7971b10781ddb509ac34577db19f04
Salman Shah <sbs.191197@gmail.com>
2016-06-29 21:00:52 +0530

    Added code
    Added more code to the edit account option.
    Created new functions admin and client.

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,22 @@ using namespace std;
 void newAccount();
 void editAccount(int, string);
 
+void admin(int, string);
+void client(int, string);
+
+char name[256];
+
 int main() {
 
     //Welcome Screen
-    for (int i = 0 ; i < 25 ; i ++)
-        printf("*");
     cout << "Hello and Welcome to E- Manager" << endl;
     cout << "Please enter your name below" << endl;
 
     //Inputting user's name to greet the user.
-    char name[256];
+
     cin.getline (name,256);
     cout << "Good day to you, " << name << endl;
+    printf("\n");
 
     //Start banking functions
     again:
@@ -35,7 +39,9 @@ int main() {
     int accnum;
     string pswd;
     cin >> choice;
+    printf("\n");
 
+    //Use of switch for any one of the options
     switch(choice) {
 
         //Case to create new account
@@ -47,23 +53,30 @@ int main() {
         //Case to edit existing account
         case 2:
             cout << "Enter the account number: " << endl;
+            cin >> accnum;
+
+            //Default account number set up.
             if(accnum == 123456) {
                 wrongpswd:
                 cout << "Enter your account's password" << endl;
                 cin >> pswd;
                 if (pswd == "nitk2015") {
+                    printf("\n");
                     editAccount(accnum,pswd);
                     }
                 else {
                     cout << "Wrong Password entered" << endl;
                     cout << "Try again" << endl;
+                    printf("\n");
                     goto wrongpswd;
                     }
                 }
+
+            //In case account number doesn't match with the file
             else {
                 cout << "Account not found. Create a new account" << endl;
                 goto again;
-            }
+                }
             break;
 
         //Case to exit the program
@@ -72,7 +85,7 @@ int main() {
 
         //In case none of the options are entered
         default:
-            cout << "Enter an appropriate choice" <<
+            cout << "Enter an appropriate choice" << endl;
             goto again;
     }
 
@@ -81,10 +94,141 @@ int main() {
 
 //Function to add new account
 void newAccount() {
-    
+
 }
 
 //Function to edit existing account
-void existingAccount(int accnum, string pswd) {
+void editAccount(int accnum, string pswd) {
 
+    again1:
+    //Account access options
+    cout << "Enter the number corresponding to ";
+    cout << "one of the following options" << endl;
+    cout << "1. Admin access" << endl;
+    cout << "2. Client access" << endl;
+
+    int choice;
+    cin >> choice;
+
+    switch(choice) {
+
+        //Admin access option
+        case 1:
+            cout << "You have chosen admin access for your account" << endl;
+            admin(accnum, pswd);
+            break;
+        //Client access option
+        case 2:
+            cout << "You have chosen client access for your account" << endl;
+            client(accnum ,pswd);
+            break;
+        //None of the above options
+        default:
+            cout << "Please select a valid option" << endl;
+            goto again1;
+
+        }
+}
+
+//Function to give admin access over an account
+void admin(int accnum, string pswd) {
+
+    again2:
+    cout << "Welcome " << name << " to your account number " << accnum << endl;
+    cout << "Select any one of the following options" << endl;
+    cout << "1. View Account Details" << endl;
+    cout << "2. Add Money to Account" << endl;
+    cout << "3. Remove Money from Account" << endl;
+    cout << "4. Exit" << endl;
+
+    int n;
+    cin >> n;
+
+    switch (n) {
+
+        //Account Viewing
+        case 1:
+            cout << "Here are the details of your account" << endl;
+            //Add code to show Account Details
+
+            break;
+
+        //Account money addition
+        case 2:
+            cout << "Here are the details of your account" << endl;
+            //Add code to show Account Details
+
+            //Add code to add money to the account
+            int addmoney;
+            cout << "Enter the amount of money to add to your account" << endl;
+            cin >> addmoney;
+
+            break;
+
+        //Account money withdrawal
+        case 3:
+            cout << "Here are the details of your account" << endl;
+            //Add code to show Account Details
+
+            //Add code to subtract money from account
+            int submoney;
+            cout << "Enter the amount of money to be removed from your account" << endl;
+            cin >> submoney;
+
+            break;
+
+        //Exit statement
+        case 4:
+            exit(0);
+
+        //None of the above options
+        default:
+            cout << "Please select a valid option" << endl;
+            goto again2;
+    }
+
+}
+
+//Function to give client access over an account
+void client(int accnum, string pswd) {
+    again3:
+    cout << "Welcome " << name << " to your account number" << endl;
+    cout << "Select any one of the following options" << endl;
+    cout << "1. View Account Details" << endl;
+    cout << "2. Add spending to Account" << endl;
+    cout << "3. Exit" << endl;
+
+    int n;
+    cin >> n;
+
+    switch(n) {
+
+        //Account Viewing
+        case 1:
+            cout << "Here are the details of your account" << endl;
+            //Add code to show Account Details
+
+            break;
+
+        //Account spending
+        case 2:
+            cout << "Here are the details of your account" << endl;
+            //Add code to show Account Details
+
+            //Add code to spend money from account
+            int spendmoney;
+            cout << "Enter the amount of money to be spent from your account" << endl;
+            cin >> spendmoney;
+
+            break;
+
+        //Exit from account
+        case 3:
+            exit(0);
+
+        //None of the above options
+        default:
+            cout << "Please select a valid option" << endl;
+            goto again3;
+    }
 }
